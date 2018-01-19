f0b23ade10fc11543bd96df8ff9f71adc4fab7ce
Ada <ayshaw@g.harvard.edu>
2018-01-19 11:01:49 -0500

    fixing some labels on identifying layers observation

--- identifying layers-observation.R
+++ identifying layers-observation.R
@@ -1,41 +1,59 @@
 rm(list=ls())
 setwd("C:\\Users\\ashaw\\Documents\\R\\EPS 236\\EPS 236 final project\\2013-2014")
+
+#packages and data required
 load("file_name.Rdata")
 require('signal');require('prospectr');require('pspline')
+
+#create list to store data
 difference=list()
 diff_mean=list()
 num_layers_obs=list()
 date=list()
+width_layers=list()
+altitude_layers=list()
+
 #some funky data on this date:
 file_name=file_name[-which(file_name=="th968_2014_06_05_fleout")]
 
-width_layers=list()
-altitude_layers=list()
 for (tick_s in 1:length(file_name)){
+  #list of raw data
   b=list()
+  
+  #loading the saved raw data
   load(paste('data_fixed\\data_fixed',file_name[tick_s],'.Rdata',sep=''))
+  
   #getting rid of missing data
   red=cbind(b$o3_mr,b$alt,b$temperature,b$wv_mr)
   blue=red[complete.cases(red), ]
-  index_tropp=which(blue[,2]>12)[1]
-  ####defining tropopause
+  
+  # index_tropp=which(blue[,2]>12)[1] will delete if code runs fine without this
+  
+  #defining altitude range for tropopause
   tprange_bot=which(blue[,2]>8)[1]
   tprange_top=which(blue[,2]>16)[1]
   plot_top=which(blue[,2]>12)[1]
   plot_bot=which(blue[,2]>2)[1]
   orange=blue[plot_bot:plot_top,]
+  
+  #if the top is an NA, we assign 11km as the tropopause
   if(is.na(tprange_top)==T){tprange_top=blue[which(blue[,2]-11<.3)[1],2]}
+  
+  #range of values to search for lapse rate change
   tprange=blue[tprange_bot:tprange_top,]
   tprange_fit=sm.spline(x=tprange[,2],y=tprange[,3],spar=9E-1)
+  
+  #using lapse rate to define tropopause
   lapse_rate=c(matrix(0,c(1,1)),diff(tprange_fit$ysmth))/c(matrix(0,c(1,1)),diff(tprange_fit$x))
   tpause=tprange_fit$x[which(abs(lapse_rate)<2)[1]]
   in_tpause=which(blue[,2]>tpause-1.5)[1]
   in_ft=which(blue[,2]>2)[1]
   purple=blue[in_ft:in_tpause,]
-  ### fit spline
+  
+  #fit spline to ozone data 
   z0=sm.spline(x=purple[,2],y=purple[,1],spar=1E-1)
   
-  
+  #find the max/min and concavity. I fitted a spline to the data to clean up stragglers
   source("finite_differences.R")
   fdx=finite.differences(z0$x,z0$ysmth)
   y=sm.spline(x=z0$x,y=fdx,spar=1E-1)
@@ -43,18 +61,19 @@ for (tick_s in 1:length(file_name)){
   fdx2=finite.differences(z0$x,y_reshape$y)
   yy=sm.spline(x=z0$x,y=fdx2,spar=1E-2)
   yy_reshape=approx(x=yy$x,y=yy$ysmth,xout=z0$x,n=length(fdx2))
-  
-  
   index0_diff1=which(diff(sign(y_reshape$y))!=0)
   index0_diff2=which(diff(sign(yy_reshape$y))!=0)
-  layers=logical(length(index0_diff1))
   purple_reshape=approx(x=purple[,2],y=purple[,1],xout=z0$x,n=length(fdx))
+ 
+   # layers=logical(length(index0_diff1)) delete if code runs w/o it
+
+  #list of variables stored in loop
   width=rep(0,length(index0_diff1))
   maxima=rep(0,length(index0_diff1))
   max_pt=rep(0,length(index0_diff1))
   altitude_layer=rep(0,length(index0_diff1))
 
-  if(length(index0_diff1)>0){  
+  if(length(index0_diff1)>0){  #make sure that you don't have a flat profile
   for (tick_alt in 1:length(index0_diff1)){
      if(index0_diff1[tick_alt]<max(index0_diff2)& index0_diff1[tick_alt]>min(index0_diff2)){
     upper_in=index0_diff2[which(index0_diff2>index0_diff1[tick_alt])[1]] 
@@ -82,7 +101,8 @@ for (tick_s in 1:length(file_name)){
   date[[tick_s]]=paste(substr(file_name[tick_s],7,10),substr(file_name[tick_s],12,13),substr(file_name[tick_s],15,16),sep='/')
   
   num_layers_obs[[tick_s]]=length(intersect(intersect(maxima_10_in,width_met_in),ppb_80_in))
-  png(paste('identifyinglayers\\identifyinglayers_adamethod',file_name[tick_s],'.png'))
+  
+  png(paste('identifyinglayers\\identifyinglayers_observation',file_name[tick_s],'.png'))
   plot(orange[,1],orange[,2],col=rgb(0.5,0.5,0.5,0.4),main=date[[tick_s]],xlab='ppmv',ylab='km',xlim=c(.03,.12),ylim=c(2,10))
   lines(z0$ysmth,z0$x)
   obs_max_o3=max_pt[intersect(intersect(maxima_10_in,width_met_in),ppb_80_in)]
